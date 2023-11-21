9b719b3a88170f5d4bf754ea756cd4dd2c0fdcd5
LunarEyeGaming <lunareyegaming@gmail.com>
2023-11-20 21:30:53 -0500

    Update voideddepthpoison.lua

--- scripts/voideddepthpoison.lua
+++ scripts/voideddepthpoison.lua
@@ -14,7 +14,9 @@ function update(dt)
     status.modifyResource("health", -(poisonAmount - poisonTolerance) * damageRate * dt)
   end
   
-  -- Display poison amount
-  status.addEphemeralEffect("v-depthpoisonindicator")
+  -- Display poison amount (if poisonAmount is positive)
+  if poisonAmount > 0 then
+    status.addEphemeralEffect("v-depthpoisonindicator")
+  end
   world.debugText("poisonAmount: %s / %s \ndamage: %s", string.format("%.1f", poisonAmount), poisonTolerance, math.max(0, (poisonAmount - poisonTolerance) * damageRate * dt), mcontroller.position(), "green")
 end
