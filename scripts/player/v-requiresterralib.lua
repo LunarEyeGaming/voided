0ed40a7d05163dc1c978af1812ee33aa1f308a80
LunarEyeGaming <lunareyegaming@gmail.com>
2025-03-26 15:24:00 -0400

    Add popup for people without TerraLib

---
+++ scripts/player/v-requiresterralib.lua
@@ -0,0 +1,53 @@
+--[[
+  Script that shows a simple warning to the player that Voided requires TerraLib if the player doesn't have it
+  installed.
+]]
+
+local showMessageInterval
+local showMessageDelay
+local showMessageTimer
+
+function init()
+  showMessageInterval = 1  -- measured in the number of seconds
+  showMessageDelay = 60  -- measured in the number of ticks
+  showMessageTimer = showMessageDelay
+
+  if storage.showAgain == nil then
+    storage.showAgain = true
+  end
+
+  message.setHandler("v-neverShowAgain", function()
+    storage.showAgain = false
+  end)
+
+  local tempInit, tempUpdate, tempShowMessage = init, update, showMessage
+  local status, _ = pcall(require, "/scripts/terra_wormbody.lua")
+
+  if not status then
+    sb.logWarn("[Voided: Expansion Mod] Could not load file '/scripts/terra_wormbody.lua'. Assuming TerraLib is not installed.")
+  else
+    init, update, showMessage = tempInit, tempUpdate, tempShowMessage
+    script.setUpdateDelta(0)  -- Don't show message
+  end
+end
+
+function update(dt)
+  if showMessageTimer then
+    showMessageTimer = showMessageTimer - 1
+    -- If the message should be shown again and either of the following is true: storage.lastShown is undefined or more
+    -- than showMessageInterval seconds has passed since the message was last shown...
+    if not messageShown and storage.showAgain and showMessageTimer <= 0 and
+        (not storage.lastShown or os.time() > storage.lastShown + showMessageInterval) then
+      showMessage()
+      showMessageTimer = nil
+    end
+  end
+end
+
+function showMessage()
+  storage.lastShown = os.time()
+  messageShown = true
+
+  local configData = root.assetJson("/interface/scripted/v-terralibwarning/v-terralibwarning.config")
+  player.interact("ScriptPane", configData, player.id())
+end
