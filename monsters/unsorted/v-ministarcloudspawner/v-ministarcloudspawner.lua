43b151fedd72c522140796bfc131fe3ea7e21aae
LunarEyeGaming <lunareyegaming@gmail.com>
2025-05-16 13:39:57 -0400

    Better Ministar Cloud spawning mechanism * Make Ministar Cloud spawning more reliable by using monsters instead * Make Ministar Clouds have a more visible disappear animation and play a sound upon disappearing * Make Ministar Clouds last for less time and take longer to disappear

---
+++ monsters/unsorted/v-ministarcloudspawner/v-ministarcloudspawner.lua
@@ -0,0 +1,43 @@
+require "/scripts/util.lua"
+require "/scripts/vec2.lua"
+require "/scripts/rect.lua"
+
+local spawnRegion
+local timeRange
+local projectileType
+local projectileDirection
+local projectileParameters
+local projectileBoundBox
+
+local timer
+
+function init()
+  spawnRegion = rect.translate(config.getParameter("spawnRegion"), mcontroller.position())
+  timeRange = config.getParameter("timeRange")
+  projectileType = config.getParameter("projectileType")
+  projectileDirection = config.getParameter("projectileDirection")
+  projectileParameters = config.getParameter("projectileParameters")
+  projectileBoundBox = config.getParameter("projectileCheckRegion")
+
+  monster.setDamageBar("None")
+  script.setUpdateDelta(10)
+
+  timer = 0
+end
+
+function update(dt)
+  timer = timer - dt
+
+  if timer < 0 then
+    local pos = rect.randomPoint(spawnRegion)
+
+    if not world.rectCollision(rect.translate(projectileBoundBox, pos)) then
+      world.spawnProjectile(projectileType, pos, nil, projectileDirection, false, projectileParameters)
+      timer = util.randomInRange(timeRange)
+    end
+  end
+end
+
+function shouldDie()
+  return false
+end
