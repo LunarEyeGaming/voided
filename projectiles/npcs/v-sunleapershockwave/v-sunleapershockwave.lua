e006c64b026e99fb2f41fa2ef712136cce05fe6d
LunarEyeGaming <lunareyegaming@gmail.com>
2025-05-27 23:48:54 -0400

    Nova Goliath progress; Sun Leaper tweaks * Make Sun Leaper shockwaves produce less glitchy trails * Make Sun Leaper shockwaves slightly more predictable * Update Sun Leaper shockwave sprite * Make Sun Leaper hop animation slightly less glitchy * Make Sun Leapers glow in the dark a bit * Make Nova Goliaths more common * Increase health of Nova Goliaths * Make Nova Goliaths more likely to activate its abilities * Make Nova Goliaths ignore sun liquid * More Nova Goliath animation progress * Make Nova Goliath blast telegraph sounds louder * Nova Goliath blasts now become intangible partway through their existence

--- projectiles/npcs/v-sunleapershockwave/v-sunleapershockwave.lua
+++ projectiles/npcs/v-sunleapershockwave/v-sunleapershockwave.lua
@@ -5,15 +5,29 @@ require "/scripts/v-world.lua"
 
 -- TODO: Fix issue encountered due to world wrap.
 
-local directions = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}}
-local currentDirectionIdx
+local DIRECTIONS = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}}
+
+local trailProjectileType  ---@type string
+local trailProjectileParameters  ---@type Json
+local trailVerticalOffset  ---@type number
+
+local currentDirectionIdx  ---@type integer
+local preferredDirection  ---@type integer
+local prevCollidePointLeft  ---@type boolean
 
 function init()
+  trailProjectileType = "v-sunleapershockwavetrail"
+  trailProjectileParameters = {
+    power = projectile.power(),
+    powerMultiplier = projectile.powerMultiplier()
+  }
+  trailVerticalOffset = 0.625
+
   local startRotation = util.wrapAngle(mcontroller.rotation())
 
   -- Choose currentDirectionIdx value to use based on current rotation.
-  for i = 1, #directions do
-    if vec2.angle(directions[i]) <= startRotation and startRotation <= vec2.angle(directions[i % #directions + 1]) then
+  for i = 1, #DIRECTIONS do
+    if vec2.angle(DIRECTIONS[i]) <= startRotation and startRotation <= vec2.angle(DIRECTIONS[i % #DIRECTIONS + 1]) then
       currentDirectionIdx = i
       break
     end
@@ -23,64 +37,91 @@ function init()
   if not currentDirectionIdx then
     currentDirectionIdx = 1
   end
+  preferredDirection = 1
 
   mcontroller.setPosition({math.floor(mcontroller.position()[1]) + 0.5, math.floor(mcontroller.position()[2]) + 0.5})
 end
 
 function update(dt)
-  local ownPos = mcontroller.position()
+  local nextPos, nextDirIdx = getNextPos(mcontroller.position())
 
-  local nextPos
+  -- If a next position was found...
+  if nextPos then
+    currentDirectionIdx = nextDirIdx  -- Set currentDirectionIdx
 
+    -- If the current direction is diagonal...
+    if currentDirectionIdx % 2 == 0 then
+      local nudgeTestDirection
+      if prevCollidePointLeft then
+        nudgeTestDirection = 1  -- Counterclockwise
+      else
+        nudgeTestDirection = -1  -- Clockwise
+      end
+      local nudgeTest, nudgedDirection = getDirection(currentDirectionIdx + nudgeTestDirection)
+      -- Determine whether to nudge the direction of the projectile.
+      if not world.pointCollision(vec2.add(nextPos, nudgeTest)) then
+        currentDirectionIdx = nudgedDirection
+      end
+    end
+
+    local leftDirection = getDirection(currentDirectionIdx + 2)  -- 90 degrees CCW
+    local collidePointLeft = world.pointCollision(vec2.add(nextPos, vec2.mul(leftDirection, 1.5)))
+
+    -- Determine shockwave direction as well as preferred direction
+    local shockwaveDirection, shockwaveIdx
+    if collidePointLeft then
+      shockwaveDirection, shockwaveIdx = getDirection(currentDirectionIdx + 4)  -- 180 degrees
+      preferredDirection = 1
+    else
+      shockwaveDirection, shockwaveIdx = DIRECTIONS[currentDirectionIdx], currentDirectionIdx
+      preferredDirection = -1
+    end
+
+    local offsetDir = getDirection(shockwaveIdx + 2)
+    local projectilePos = vec2.add(nextPos, vec2.mul(offsetDir, trailVerticalOffset))
+    world.spawnProjectile(trailProjectileType, projectilePos, projectile.sourceEntity(), shockwaveDirection, false, trailProjectileParameters)
+
+    mcontroller.setRotation(vec2.angle(DIRECTIONS[currentDirectionIdx]))  -- Update rotation
+    mcontroller.setPosition(nextPos)
+
+    prevCollidePointLeft = collidePointLeft
+  end
+end
+
+---Returns the direction at index `index` followed by the wrapped index.
+---@param index integer
+---@return Vec2F
+---@return integer
+function getDirection(index)
+  local wrappedIdx = (index - 1) % #DIRECTIONS + 1
+  -- Wrapped index access
+  return DIRECTIONS[wrappedIdx], wrappedIdx
+end
+
+---Returns the next position to go to and the corresponding index used to test it.
+---@param pos Vec2F
+---@return table?
+---@return integer?
+function getNextPos(pos)
   -- Search through all possible directions for an empty position adjacent to at least one block.
-  for i = 0, #directions do
+  for i = 0, #DIRECTIONS do
     -- Alternate between counterclockwise and clockwise rotation (as directions are ordered in a counterclockwise
     -- manner).
     local multiplier
     if i % 2 == 0 then
-      multiplier = 1
+      multiplier = preferredDirection
     else
-      multiplier = -1
+      multiplier = -preferredDirection
     end
-    -- We integer-divide i by 2 to avoid skipping any positions. Also apply modulus after offsetting to wrap around.
-    local testIdx = (currentDirectionIdx + i // 2 * multiplier - 1) % #directions + 1
+    -- We integer-divide i by 2 to avoid skipping any positions.
+    local testDirection, testIdx = getDirection(currentDirectionIdx + i // 2 * multiplier)
 
-    -- Get corresponding direction and add it to ownPos to get the test position.
-    local testPos = vec2.add(ownPos, directions[testIdx])
+    -- Get corresponding direction and add it to pos to get the test position.
+    local testPos = vec2.add(pos, testDirection)
 
     -- If the position is empty and is adjacent to some tiles...
     if not world.pointCollision(testPos) and vWorld.isGroundAdjacent(testPos) then
-      currentDirectionIdx = testIdx  -- Set currentDirectionIdx
-      nextPos = testPos  -- Set next position
-      mcontroller.setRotation(vec2.angle(directions[currentDirectionIdx]))  -- Update rotation
-      break  -- Exit loop
+      return testPos, testIdx
     end
   end
-
-  -- If a next position was found...
-  if nextPos then
-    local leftDirection = getDirection(currentDirectionIdx + 2)
-    -- local rightDirection = directions[(currentDirectionIdx - 2 - 1) % #directions + 1]
-    local collidePointLeft = world.pointCollision(vec2.add(nextPos, leftDirection))
-    -- local collidePointRight = world.pointCollision(vec2.add(nextPos, rightDirection))
-
-    local shockwaveDirection
-    if collidePointLeft then
-      shockwaveDirection = getDirection(currentDirectionIdx + 4)  -- 180 degrees
-    else
-      shockwaveDirection = directions[currentDirectionIdx]
-    end
-    world.spawnProjectile("v-fireshockwavenoflip", nextPos, projectile.sourceEntity(), shockwaveDirection, false, {
-      power = projectile.power(),
-      powerMultiplier = projectile.powerMultiplier()
-    })
-    mcontroller.setPosition(nextPos)
-  else
-    -- projectile.die()
-  end
-end
-
-function getDirection(index)
-  -- Wrapped index access
-  return directions[(index - 1) % #directions + 1]
 end
