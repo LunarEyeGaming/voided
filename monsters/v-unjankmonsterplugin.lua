4eb8bd3cfe301fc93e8d882acf8d6fd6212ba30a
LunarEyeGaming <lunareyegaming@gmail.com>
2026-01-23 00:58:37 -0500

    Fix v-unjankmonsterplugin.lua spamming errors

--- monsters/v-unjankmonsterplugin.lua
+++ monsters/v-unjankmonsterplugin.lua
@@ -1,11 +1,13 @@
 local oldDie = die or function() end
 
 function die()
-  local board = self.deathBehavior:blackboard()
-  board:setEntity("self", entity.id())
-  board:setPosition("self", mcontroller.position())
-  board:setNumber("dt", script.updateDt())
-  board:setNumber("facingDirection", self.facingDirection or mcontroller.facingDirection())
+  if self.deathBehavior then
+    local board = self.deathBehavior:blackboard()
+    board:setEntity("self", entity.id())
+    board:setPosition("self", mcontroller.position())
+    board:setNumber("dt", script.updateDt())
+    board:setNumber("facingDirection", self.facingDirection or mcontroller.facingDirection())
+  end
 
   oldDie()
 end
