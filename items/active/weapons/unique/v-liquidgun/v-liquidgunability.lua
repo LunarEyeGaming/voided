b2137a9e59569bb6d87281692fd8c1678dbae7a8
LunarEyeGaming <lunareyegaming@gmail.com>
2026-05-12 21:51:35 -0400

    liquidgun (WIP)

---
+++ items/active/weapons/unique/v-liquidgun/v-liquidgunability.lua
@@ -0,0 +1,170 @@
+require "/items/active/weapons/ranged/gunfire.lua"
+
+require "/scripts/v-animator.lua"
+
+LiquidGunAbility = GunFire:new()
+
+function LiquidGunAbility:init()
+  GunFire.init(self)
+
+  if not world.isTileProtected(mcontroller.position()) then
+    self.energyUsage = 0
+  else
+    self.inProtectedArea = true
+  end
+  self.currentLiquid = self:getLiquid()
+  self.active = false
+end
+
+function LiquidGunAbility:update(dt, fireMode, shiftHeld)
+  WeaponAbility.update(self, dt, fireMode, shiftHeld)
+
+  self.currentLiquid = self:getLiquid()
+
+  self.cooldownTimer = math.max(0, self.cooldownTimer - self.dt)
+
+  if animator.animationState("firing") ~= "fire" then
+    animator.setLightActive("muzzleFlash", false)
+  end
+
+  if self.fireMode == (self.activatingFireMode or self.abilitySlot)
+    and not self.weapon.currentAbility
+    and self.cooldownTimer == 0
+    and not world.lineTileCollision(mcontroller.position(), self:firePosition()) then
+
+    if self.fireType == "auto" and self:consumeLiquid() then
+      self:setState(self.auto)
+    elseif self.fireType == "burst" then
+      self:setState(self.burst)
+    end
+  end
+
+  if self.weapon.currentAbility == self then
+    if not self.active then self:activate() end
+  elseif self.active then
+    self:deactivate()
+  end
+end
+
+function LiquidGunAbility:fireProjectile(projectileType, projectileParams, inaccuracy, firePosition, projectileCount)
+  local params = sb.jsonMerge(self.projectileParameters, projectileParams or {})
+  params.power = self:damagePerShot()
+  params.powerMultiplier = activeItem.ownerPowerMultiplier()
+  params.speed = util.randomInRange(params.speed)
+
+  -- Create undefined parameters
+  -- if not params.actionOnReap then
+  --   params.actionOnReap = {}
+  -- end
+  if not params.periodicActions then
+    params.periodicActions = {}
+  end
+
+  local itemConfig = root.itemConfig(self.currentLiquid)
+  if not itemConfig.config.liquid then
+    error("Item config for " .. self.currentLiquid .. " does not contain 'liquid'")
+  end
+  -- table.insert(params.actionOnReap, {
+  --   action = "liquid",
+  --   quantity = self.projectileLiquidQuantity,
+  --   liquid = itemConfig.config.liquid
+  -- })
+  params.liquidPlacer = {
+    name = itemConfig.config.liquid,
+    placeOnDestroy = true
+  }
+
+
+  local liquidAttributes = self:getLiquidAttributes(itemConfig.config.liquid)
+
+  if type(self.projectileParticle) ~= "table" then
+    error("projectileParticle is not an object (must define the particle in the activeitem file directly)")
+  end
+
+  local particle = sb.jsonMerge(self.projectileParticle, {
+    color = liquidAttributes.color,
+    light = liquidAttributes.glowColor
+  })
+  params.processing = "?multiply=" .. vAnimator.colorToString(liquidAttributes.color)
+  -- params.statusEffects = liquidAttributes.statusEffects
+
+  table.insert(params.periodicActions, {
+    time = self.projectileParticleInterval,
+    action = "particle",
+    rotate = true,
+    specification = particle
+  })
+
+  if not projectileType then
+    projectileType = self.projectileType
+  end
+  if type(projectileType) == "table" then
+    projectileType = projectileType[math.random(#projectileType)]
+  end
+
+  local projectileId = 0
+  for i = 1, (projectileCount or self.projectileCount) do
+    if params.timeToLive then
+      params.timeToLive = util.randomInRange(params.timeToLive)
+    end
+
+    projectileId = world.spawnProjectile(
+        projectileType,
+        firePosition or self:firePosition(),
+        activeItem.ownerEntityId(),
+        self:aimVector(inaccuracy or self.inaccuracy),
+        false,
+        params
+      )
+  end
+  return projectileId
+end
+
+function LiquidGunAbility:activate()
+  self.active = true
+  animator.playSound("fireStart")
+  animator.playSound("fireLoop", -1)
+end
+
+function LiquidGunAbility:deactivate()
+  self.active = false
+  animator.stopAllSounds("fireStart")
+  animator.stopAllSounds("fireLoop")
+  animator.playSound("fireEnd")
+end
+
+---Attempts to consume a liquid from the player's inventory. Returns the liquid consumed, or `nil` if none was consumed.
+---If in a protected area, consumes energy instead.
+function LiquidGunAbility:consumeLiquid()
+  if self.inProtectedArea then
+    return (self.energyUsage <= 0 or not status.resourceLocked("energy"))
+    and status.overConsumeResource("energy", self:energyPerShot())
+  end
+  local liquid = self:getLiquid()
+
+  if liquid then
+    liquid.count = 1  -- Ask to consume one unit of liquid.
+    player.consumeItem(liquid)
+  end
+
+  return liquid
+end
+
+---Returns a liquid from the player's inventory.
+function LiquidGunAbility:getLiquid()
+  return player.getItemWithParameter("category", "liquid")
+end
+
+function LiquidGunAbility:getLiquidAttributes(liquid)
+  local liquidConfig = root.liquidConfig(liquid)
+
+  if not liquidConfig then
+    error("Invalid liquid: " .. liquid)
+  end
+
+  return {
+    color = liquidConfig.config.color,
+    glowColor = liquidConfig.config.radiantLight,
+    statusEffects = liquidConfig.config.statusEffects
+  }
+end
---
+++ items/active/weapons/unique/v-liquidgun/v-liquidgunaltability.lua
@@ -0,0 +1,100 @@
+require "/scripts/v-animator.lua"
+
+LiquidGunAltAbility = WeaponAbility:new()
+
+function LiquidGunAltAbility:init()
+  self.active = false
+end
+
+function LiquidGunAltAbility:update(dt, fireMode, shiftHeld)
+  WeaponAbility.update(self, dt, fireMode, shiftHeld)
+
+  if self.fireMode == (self.activatingFireMode or self.abilitySlot)
+    and not self.weapon.currentAbility then
+
+    self:setState(self.collectingState)
+  end
+end
+
+function LiquidGunAltAbility:collectingState()
+  local collectTimer = self.collectInterval
+
+  self:activate()
+
+  while self.fireMode == (self.activatingFireMode or self.abilitySlot) and status.overConsumeResource("energy", (self.energyUsage or 0) * self.dt) do
+    -- Periodically collect liquid.
+    collectTimer = collectTimer - self.dt
+    if collectTimer <= 0 then
+      self:collectLiquid(activeItem.ownerAimPosition(), self.collectRadius)
+
+      collectTimer = self.collectInterval
+    end
+
+    coroutine.yield()
+  end
+
+  self:deactivate()
+end
+
+function LiquidGunAltAbility:collectLiquid(position, radius)
+  if world.lineCollision(mcontroller.position(), position) then return end
+
+  if world.magnitude(mcontroller.position(), position) > self.maxCollectRange then return end
+
+  if radius and radius > 0 then
+    for x = -radius, radius do
+      for y = -radius, radius do
+        local offset = {x, y}
+        if vec2.mag(offset) <= radius then
+          local pos = vec2.add(position, offset)
+          self:collectOneLiquid(pos)
+        end
+      end
+    end
+  else
+    self:collectOneLiquid(position)
+  end
+end
+
+function LiquidGunAltAbility:collectOneLiquid(position)
+  local liquid = world.liquidAt(position)
+
+  if world.isTileProtected(position) then
+    return
+  end
+
+  -- Don't collect if there isn't enough liquid.
+  if not liquid or liquid[2] < self.collectThreshold then
+    return
+  end
+
+  local liquidConfig = root.liquidConfig(liquid[1])
+
+  -- Don't collect if the liquid isn't real or it doesn't have an item.
+  if not liquidConfig or not liquidConfig.config.itemDrop then return end
+
+  -- Consume some liquid, putting any leftover liquid back.
+  local consumedLiquid = world.destroyLiquid(position)
+  if consumedLiquid and consumedLiquid[2] > 1 then
+    world.spawnLiquid(position, consumedLiquid[1], consumedLiquid[2] - 1)
+  end
+
+  -- Give the liquid to the player.
+  player.giveItem(liquidConfig.config.itemDrop)
+end
+
+function LiquidGunAltAbility:activate()
+  self.active = true
+  animator.playSound("fireStart")
+  animator.playSound("fireLoop", -1)
+end
+
+function LiquidGunAltAbility:deactivate()
+  self.active = false
+  animator.stopAllSounds("fireStart")
+  animator.stopAllSounds("fireLoop")
+  animator.playSound("fireEnd")
+end
+
+function LiquidGunAltAbility:uninit()
+end
