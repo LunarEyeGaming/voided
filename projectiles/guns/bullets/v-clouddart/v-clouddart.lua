a88baa4c46e4cbabc3ccb112bff78dd87f1c5c85
LunarEyeGaming <lunareyegaming@gmail.com>
2026-05-17 18:31:48 -0400

    Simplify cloud dart collision detection

--- projectiles/guns/bullets/v-clouddart/v-clouddart.lua
+++ projectiles/guns/bullets/v-clouddart/v-clouddart.lua
@@ -1,36 +1,34 @@
 require "/scripts/vec2.lua"
 
+local stuckProjectile
 local isStuck
-local prevPosition
 
 function init()
+  stuckProjectile = config.getParameter("stuckProjectile")
   isStuck = false
-  prevPosition = mcontroller.position()
 end
 
 function update(dt)
   if not isStuck then
-    -- Make a guess about the entities that the projectile has hit.
-    local queried = world.entityLineQuery(prevPosition, mcontroller.position(), {includedTypes = {"creature"}})
-
-    for _, entityId in ipairs(queried) do
-      if world.entityCanDamage(projectile.sourceEntity(), entityId) then
-        world.spawnProjectile("v-clouddartstuck", mcontroller.position(), projectile.sourceEntity(), vec2.withAngle(mcontroller.rotation()), false, {
-          power = projectile.power(),
-          powerMultiplier = projectile.powerMultiplier(),
-          timeToLive = projectile.timeToLive(),
-          stuckEntityId = entityId,
-          -- Offset relative to entity.
-          stuckOffset = world.distance(prevPosition, world.nearestTo(prevPosition, world.entityPosition(entityId)))
-        })
-
-        projectile.die()
-
-        return  -- This skips all other entities and stops the check for sticky collision.
-      end
-    end
 
     isStuck = mcontroller.stickingDirection() ~= nil
-    prevPosition = mcontroller.position()
   end
+end
+
+function hit(entityId)
+  -- Avoid sticking to enemies if already stuck to terrain.
+  if isStuck then
+    return
+  end
+
+  world.spawnProjectile(stuckProjectile, mcontroller.position(), projectile.sourceEntity(), vec2.withAngle(mcontroller.rotation()), false, {
+    power = projectile.power(),
+    powerMultiplier = projectile.powerMultiplier(),
+    timeToLive = projectile.timeToLive(),
+    stuckEntityId = entityId,
+    -- Offset relative to entity.
+    stuckOffset = world.distance(mcontroller.position(), world.nearestTo(mcontroller.position(), world.entityPosition(entityId)))
+  })
+
+  projectile.die()
 end
