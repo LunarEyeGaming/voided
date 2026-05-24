242a35c2df383ce6412bf62e8b5439b46cd4fae3
LunarEyeGaming <lunareyegaming@gmail.com>
2026-05-23 23:20:14 -0400

    Add Giant Solar Lens beam animation

--- objects/dungeon/v-solarspire/v-giantsolarlens/v-giantsolarlens.lua
+++ objects/dungeon/v-solarspire/v-giantsolarlens/v-giantsolarlens.lua
@@ -1,5 +1,11 @@
 require "/scripts/vec2.lua"
 
+local beamOscillateMinAmplitude
+local beamOscillateMaxAmplitude
+local beamOscillatePeriod
+
+local beamOscillateTimer
+
 function init()
   local direction = config.getParameter("direction")
   local angle
@@ -11,4 +17,17 @@ function init()
   angle = angle + config.getParameter("angleOffset", 0) * math.pi / 180
   animator.resetTransformationGroup("lens")
   animator.rotateTransformationGroup("lens", angle, config.getParameter("center"))
+
+  beamOscillateMinAmplitude = config.getParameter("beamOscillateMinAmplitude", 0.5)
+  beamOscillateMaxAmplitude = config.getParameter("beamOscillateMaxAmplitude", 1)
+  beamOscillatePeriod = config.getParameter("beamOscillatePeriod", 1)
+
+  beamOscillateTimer = 0
+end
+
+function update(dt)
+  beamOscillateTimer = (beamOscillateTimer + dt) % beamOscillatePeriod
+  animator.resetTransformationGroup("beam")
+  local xScale = beamOscillateMinAmplitude + (beamOscillateMaxAmplitude - beamOscillateMinAmplitude) * (math.cos(2 * math.pi * beamOscillateTimer / beamOscillatePeriod) / 2 + 0.5)
+  animator.scaleTransformationGroup("beam", {xScale, 1})
 end
