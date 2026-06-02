b5d29c4e946d7d1ec35fea5bb37a48e4f7601fef
LunarEyeGaming <lunareyegaming@gmail.com>
2026-06-02 00:15:28 -0400

    v-nitrogenfreezing fix * The warmth decreasing properties of Liquid Nitrogen no longer uses a separate status effect

--- stats/effects/v-nitrogenfreezing/v-nitrogenfreezing.lua
+++ stats/effects/v-nitrogenfreezing/v-nitrogenfreezing.lua
@@ -1,3 +1,8 @@
+local heatLossRate
+local damageTickTime
+
+local damageTickTimer
+
 function init()
   animator.setParticleEmitterOffsetRegion("snow", mcontroller.boundBox())
   animator.setParticleEmitterActive("snow", true)
@@ -8,20 +13,33 @@ function init()
 
   self.healthDamage = config.getParameter("healthDamage", 1)
 
-  script.setUpdateDelta(config.getParameter("tickRate", 1))
+  damageTickTime = config.getParameter("damageTickTime")
 
-  effect.addStatModifierGroup({{stat = "energyRegenPercentageRate", effectiveMultiplier = 0}})
+  heatLossRate = config.getParameter("heatLossRate")
 
+  damageTickTimer = damageTickTime
 end
 
 function update(dt)
   mcontroller.controlModifiers(self.movementModifiers)
+
+  damageTickTimer = damageTickTimer - dt
+  if damageTickTimer <= 0 then
     status.applySelfDamageRequest({
-        damageType = "IgnoresDef",
-        damage = self.healthDamage,
-        damageSourceKind = "ice",
-        sourceEntityId = entity.id()
-      })
+      damageType = "IgnoresDef",
+      damage = self.healthDamage,
+      damageSourceKind = "ice",
+      sourceEntityId = entity.id()
+    })
+    damageTickTimer = damageTickTime
+  end
+
+  if not status.isResource("v-warmth") then
+    status.addEphemeralEffect("v-simulatedwarmth")
+    world.sendEntityMessage(entity.id(), "v-simulatedwarmth-consume", heatLossRate * dt)
+  else
+    status.overConsumeResource("v-warmth", heatLossRate * dt)
+  end
 end
 
 function onExpire()
