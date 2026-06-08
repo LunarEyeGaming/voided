2b1c79828da05bc54b2ed319fccc5c4c57a6d504
LunarEyeGaming <lunareyegaming@gmail.com>
2026-06-08 00:24:19 -0400

    Rift zone tile placing

--- monsters/unsorted/v-riftzone/v-riftzone.lua
+++ monsters/unsorted/v-riftzone/v-riftzone.lua
@@ -1,7 +1,9 @@
 require "/scripts/vec2.lua"
 require "/scripts/interp.lua"
 require "/scripts/util.lua"
+
 require "/scripts/v-animator.lua"
+require "/scripts/v-vec2.lua"
 
 local scanRadius
 local tileSofteningRadius
@@ -12,6 +14,7 @@ local disappearTime
 local disappearDelay
 local timeToLive
 local velocity
+local playerProximityRegion
 
 local prevPos
 local prevRadius
@@ -20,6 +23,15 @@ local currentTileSofteningRadius
 local existenceTimer
 
 local lightningController
+local perlinSource
+local orePerlinSource
+local oreBGPerlinSource
+local placedBlocks
+local placedBlocksBG
+local placedAssists
+local blocksToPlace
+local oresToPlace
+local bgOresToPlace
 
 local state
 
@@ -39,8 +51,9 @@ function init()
   appearTime = 7
   disappearTime = 7
   disappearDelay = 3
-  timeToLive = 300
+  timeToLive = config.getParameter("timeToLive", 300)
   velocity = config.getParameter("velocity", {0, 0})
+  playerProximityRegion = {-100, -100, 100, 100}
 
   prevPos = vec2.floor(mcontroller.position())
   prevRadius = 0
@@ -60,6 +73,27 @@ function init()
     startOC = cfg.startOutlineColor,
     endOC = cfg.endOutlineColor,
   }
+  perlinSource = sb.makePerlinSource({
+    seed = 0,
+    octaves = 3,
+    type = "perlin",
+    frequency = 0.05
+  })
+  orePerlinSource = sb.makePerlinSource({
+    seed = 1,
+    type = "perlin",
+    frequency = 0.1,
+    bias = -0.35
+  })
+  oreBGPerlinSource = sb.makePerlinSource({
+    seed = 2,
+    type = "perlin",
+    frequency = 0.1,
+    bias = -0.4
+  })
+  placedBlocks = {}
+  placedBlocksBG = {}
+  placedAssists = {}
 
   monster.setDamageBar("None")
   state = FSM:new()
@@ -71,16 +105,60 @@ function update(dt)
 
   lightningController:update(dt)
 
-  updateMatMods(currentScanRadius)
+  -- Suppress updates to tiles if too far away from any players to prevent loading chunks, causing a chain reaction of
+  -- rift zones loading into existence.
+  if closeToAPlayer(mcontroller.position()) then
+    updateMatMods(currentScanRadius)
+  end
 
   applyRiftDestabilization(currentScanRadius)
   applySoftenedTiles(currentTileSofteningRadius)
 
   crackleLightning(currentTileSofteningRadius)
 
-  monster.setAnimationParameter("riftSize", currentScanRadius)
+  perlinNoiseTest(currentScanRadius)
+
+  monster.setAnimationParameter("riftSize", currentScanRadius + 1)
+
+  local placedBlocksCount = 0
+  local placedBlocksBGCount = 0
+  local placedAssistsCount = 0
+  for _, _ in pairs(placedBlocks) do
+    placedBlocksCount = placedBlocksCount + 1
+  end
+  for _, _ in pairs(placedBlocksBG) do
+    placedBlocksBGCount = placedBlocksBGCount + 1
+  end
+  for _, _ in pairs(placedAssists) do
+    placedAssistsCount = placedAssistsCount + 1
+  end
+  world.debugText("placedBlocks: %s\nplacedBlocksBG: %s\nplacedAssists: %s", placedBlocksCount, placedBlocksBGCount, placedAssistsCount, mcontroller.position(), "green")
 
   -- world.loadRegion(rect.translate({-32, -32, 32, 32}, mcontroller.position()))
+end
+
+function shouldDie()
+  return g_shouldDieVar
+end
+
+function uninit()
+  clearMatMods(scanRadius)
+  clearPlacedBlocks(placedBlocks, "foreground")
+  clearPlacedBlocks(placedBlocksBG, "background")
+
+  if g_shouldDieVar then
+    return
+  end
+
+  local riftZones = world.getProperty("v-riftZones") or jarray()
+  table.insert(riftZones, {
+    position = mcontroller.position(),
+    velocity = velocity,
+    stateData = {
+      deathTime = world.time() + existenceTimer
+    }
+  })
+  world.setProperty("v-riftZones", riftZones)
 end
 
 states = {}
@@ -224,6 +302,10 @@ function updateMatMods(radius)
   local ownPos = vec2.floor(mcontroller.position())
   world.debugPoint(ownPos, "green")
 
+  placeOres()
+
+  placeBlocks()
+
   for x = -radius, radius do
     for y = -radius, radius do
       local frontScanPos = vec2.add(ownPos, {x, y})
@@ -233,9 +315,19 @@ function updateMatMods(radius)
       if frontScanDist <= radius and frontScanDist2 > prevRadius then
         world.debugPoint(frontScanPos, "green")
         attemptPlaceMatMod(frontScanPos)
+        if not world.material(frontScanPos, "foreground") and shouldPlaceBlock(frontScanPos) then
+          table.insert(blocksToPlace, frontScanPos)
+          placedBlocks[vVec2.iToString(frontScanPos)] = true
+          placedBlocksBG[vVec2.iToString(frontScanPos)] = true
+        end
       end
     end
   end
+
+  placeAssists()
+
+  local blocksToRemove = {}
+  local blocksToRemoveBG = {}
 
   for x = -radius, radius do
     for y = -radius, radius do
@@ -246,12 +338,121 @@ function updateMatMods(radius)
       if backScanDist > radius and backScanDist2 <= prevRadius then
         world.debugPoint(backScanPos, "green")
         attemptRemoveMatMod(backScanPos)
+        if placedBlocks[vVec2.iToString(backScanPos)] then
+          table.insert(blocksToRemove, backScanPos)
+          placedBlocks[vVec2.iToString(backScanPos)] = nil
+        end
+        if placedBlocksBG[vVec2.iToString(backScanPos)] then
+          table.insert(blocksToRemoveBG, backScanPos)
+          placedBlocksBG[vVec2.iToString(backScanPos)] = nil
+        end
       end
     end
   end
 
+  world.damageTiles(blocksToRemove, "foreground", mcontroller.position(), "blockish", 2 ^ 32, 0)
+  world.damageTiles(blocksToRemoveBG, "background", mcontroller.position(), "blockish", 2 ^ 32, 0)
+
   prevPos = ownPos
   prevRadius = radius
+end
+
+function placeOres()
+  if oresToPlace then
+    for _, block in ipairs(oresToPlace) do
+      world.placeMod(block, "foreground", visibleOre)
+    end
+  end
+
+  oresToPlace = {}
+
+  if bgOresToPlace then
+    for _, block in ipairs(bgOresToPlace) do
+      world.placeMod(block, "background", visibleOre)
+    end
+  end
+
+  bgOresToPlace = {}
+end
+
+function placeBlocks()
+  -- Block placement is deferred to the next tick to wait for place assists to spawn.
+  if blocksToPlace then
+    -- Sort in descending order by y value
+    table.sort(blocksToPlace, function(a, b) return a[2] > b[2] end)
+
+    for _, block in ipairs(blocksToPlace) do
+      local blockString = vVec2.iToString(block)
+      if not world.placeMaterial(block, "foreground", "v-voidstone2") and not placedAssists[blockString] then
+        placedBlocks[blockString] = nil
+      end
+      if not world.placeMaterial(block, "background", "v-voidstone2") then
+        placedBlocksBG[blockString] = nil
+      end
+      if orePerlinSource:get(block[1], block[2]) > 0 then
+        table.insert(oresToPlace, block)
+      end
+      if oreBGPerlinSource:get(block[1], block[2]) > 0 then
+        table.insert(bgOresToPlace, block)
+      end
+    end
+  end
+
+  blocksToPlace = {}
+end
+
+function placeAssists()
+  local maxYPositions = {}
+
+  placedAssists = {}
+
+  for _, block in ipairs(blocksToPlace) do
+    if not maxYPositions[block[1]] then
+      maxYPositions[block[1]] = -math.huge
+    end
+
+    maxYPositions[block[1]] = math.max(maxYPositions[block[1]], block[2])
+  end
+
+  for x, y in pairs(maxYPositions) do
+    local placedObject = world.placeObject("terra_placeassist", {x, y + 1} --[[@as Vec2I]], nil, {
+      material = "v-voidstone2",
+      overlap = true,
+      layer = "foreground"
+    })
+    if placedObject then
+      placedAssists[vVec2.iToString({x, y})] = true
+      -- placedBlocks[vVec2.iToString({x, y})] = true
+    end
+  end
+end
+
+function shouldPlaceBlock(pos)
+  local z = perlinSource:get(pos[1], pos[2])
+  if 0.1 <= z and z <= 0.2 then
+    z = 1
+  else
+    z = 0
+  end
+  -- local color = vAnimator.lerpColorRGB(z, {0, 0, 0}, {255, 255, 255})
+  -- world.debugPoint(pos, "#" .. vAnimator.colorToString(color))
+
+  return z > 0
+end
+
+function removeBlock(pos)
+
+end
+
+function perlinNoiseTest(radius)
+  local ownPos = vec2.floor(mcontroller.position())
+
+  for x = -radius, radius do
+    for y = -radius, radius do
+      local pos = vec2.add(ownPos, {x, y})
+
+    end
+  end
 end
 
 function fillMatMods(radius)
@@ -291,32 +492,49 @@ function attemptPlaceMatMod(pos)
   if world.mod(pos, "foreground") == invisibleOre then
     world.placeMod(pos, "foreground", visibleOre)
   end
+  if world.mod(pos, "background") == invisibleOre then
+    world.placeMod(pos, "background", visibleOre)
+  end
 end
 
 function attemptRemoveMatMod(pos)
   if world.mod(pos, "foreground") == visibleOre then
     world.placeMod(pos, "foreground", invisibleOre)
   end
+  if world.mod(pos, "background") == visibleOre then
+    world.placeMod(pos, "background", invisibleOre)
+  end
 end
 
-function shouldDie()
-  return g_shouldDieVar
-end
-
-function uninit()
-  clearMatMods(scanRadius)
-
-  if g_shouldDieVar then
-    return
+function clearPlacedBlocks(blocks, layer)
+  local blocksToClear = {}
+  if not blocks then
+    sb.logError("v-riftzone.lua: clearPlacedBlocks called with no blocks defined")
+  end
+  for posString, _ in pairs(blocks) do
+    local pos = vVec2.iFromString(posString)
+    table.insert(blocksToClear, pos)
   end
 
-  local riftZones = world.getProperty("v-riftZones") or jarray()
-  table.insert(riftZones, {
-    position = mcontroller.position(),
-    velocity = velocity,
-    stateData = {
-      deathTime = world.time() + existenceTimer
-    }
-  })
-  world.setProperty("v-riftZones", riftZones)
+  if #blocksToClear > 1000 then
+    blocksToClear[1001] = nil
+  end
+
+  world.damageTiles(blocksToClear, layer, mcontroller.position(), "blockish", 2 ^ 32, 0)
+end
+
+function closeToAPlayer(position)
+  local players = world.players()
+  for _, playerId in ipairs(players) do
+    local playerPos = world.entityPosition(playerId)
+    if playerPos then
+      local region = rect.translate(playerProximityRegion, playerPos)
+      if region[1] <= position[1] and position[1] <= region[3]
+        and region[2] <= position[2] and position[2] <= region[4] then
+        return true
+      end
+    end
+  end
+
+  return false
 end
