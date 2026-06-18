a5f594fc670ad8fc8dfdb304cde81e4dc1535959
LunarEyeGaming <lunareyegaming@gmail.com>
2026-06-18 16:15:45 -0400

    Aurorite armor (WIP)

---
+++ stats/effects/v-setbonuses/v-aurorite/v-auroritesetbonus.lua
@@ -0,0 +1,129 @@
+require "/scripts/status.lua"
+require "/scripts/util.lua"
+
+local level
+local hitInvulnerabilityTime
+local protectionProjectileType
+local protectionIntangibleProjectileType
+local protectionProjectileConfig
+local projectileId
+local maxHits
+local shieldRechargeTime
+local maxAbsorption
+
+local hitCounter
+local hitTimer
+local state
+local hasTakenDamage
+local currentAbsorption
+local prevAbsorption
+
+function init()
+  -- Initialize parameters
+  level = config.getParameter("level", 1)
+  hitInvulnerabilityTime = 1.0
+  protectionProjectileType = "v-auroriteshield"
+  protectionIntangibleProjectileType = "v-auroriteshieldintangible"
+  protectionProjectileConfig = config.getParameter("protectionProjectileConfig", {})
+  protectionProjectileConfig.power = (protectionProjectileConfig.power or 10) * root.evalFunction("weaponDamageLevelMultiplier", level)
+  maxHits = 3
+  shieldRechargeTime = 5
+  maxAbsorption = 200
+
+  -- Initialize variables
+  hitCounter = 0
+  hitTimer = hitInvulnerabilityTime
+  prevAbsorption = 0
+  state = FSM:new()
+  state:set(states.inactive)
+end
+
+function update(dt)
+  currentAbsorption = status.resource("damageAbsorption")
+
+  hitTimer = hitTimer - dt
+
+  hasTakenDamage = currentAbsorption < prevAbsorption
+
+  state:update(dt)
+
+  prevAbsorption = status.resource("damageAbsorption")
+end
+
+function onExpire()
+  killProjectile()
+end
+
+function killProjectile()
+  if projectileId and world.entityExists(projectileId) then
+    world.sendEntityMessage(projectileId, "kill")
+  end
+end
+
+states = {}
+
+function states.inactive()
+  killProjectile()
+  status.setResource("damageAbsorption", 0)
+
+  while not mcontroller.crouching() do
+    world.debugText("Inactive", mcontroller.position(), "green")
+    coroutine.yield()
+  end
+
+  state:set(states.activeIntangible)
+end
+
+function states.activeIntangible()
+  projectileId = world.spawnProjectile(protectionIntangibleProjectileType, mcontroller.position(), entity.id(), {1, 0}, false, protectionProjectileConfig)
+
+  while mcontroller.crouching() and hitTimer > 0 do
+    world.debugText("Active (intangible)", mcontroller.position(), "green")
+    coroutine.yield()
+  end
+
+  killProjectile()
+
+  if not mcontroller.crouching() then
+    state:set(states.inactive)
+  else
+    state:set(states.active)
+  end
+end
+
+function states.active()
+  projectileId = world.spawnProjectile(protectionProjectileType, mcontroller.position(), entity.id(), {1, 0}, false, protectionProjectileConfig)
+  status.setResource("damageAbsorption", maxAbsorption)
+
+  while mcontroller.crouching() and world.entityExists(projectileId) and not hasTakenDamage do
+    world.debugText("Active (%s hits)", hitCounter, mcontroller.position(), "green")
+    coroutine.yield()
+  end
+
+  if not world.entityExists(projectileId) or hasTakenDamage then
+    killProjectile()  -- In the event that hasTakenDamage is true
+
+    hitTimer = hitInvulnerabilityTime
+    hitCounter = hitCounter + 1
+    if hitCounter >= maxHits then
+      state:set(states.broken)
+    else
+      status.addEphemeralEffect("invulnerable", hitInvulnerabilityTime)
+      state:set(states.activeIntangible)
+    end
+  else
+    state:set(states.inactive)
+  end
+end
+
+function states.broken()
+  status.setResource("damageAbsorption", 0)
+
+  util.wait(shieldRechargeTime, function()
+    world.debugText("Broken", mcontroller.position(), "green")
+  end)
+
+  hitCounter = 0
+
+  state:set(states.inactive)
+end
