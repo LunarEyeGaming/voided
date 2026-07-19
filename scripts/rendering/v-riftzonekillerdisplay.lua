b9bc813ea78649fdc91c09661a5a0fca619efe3c
LunarEyeGaming <lunareyegaming@gmail.com>
2026-07-18 22:06:42 -0400

    Spacetime Stabilizer additions * Add background variant * Display the area of effect on the preview (currently oSB only) * Add to tileset

--- scripts/rendering/v-riftzonekillerdisplay.lua
+++ scripts/rendering/v-riftzonekillerdisplay.lua
@@ -23,8 +23,6 @@ function update(dt)
   if heldItem then
     local itemConfig = root.itemConfig(heldItem.name)
     if itemConfig.config["v-isRiftZoneKiller"] or itemConfig.parameters["v-isRiftZoneKiller"] then
-      local predictedPos = vEntity.predictPosition(dt)
-
       local queried = world.entityQuery(entity.position(), queryRange, {
         includedTypes = {"object"}
       })
@@ -32,16 +30,29 @@ function update(dt)
       for _, entityId in ipairs(queried) do
         if world.getObjectParameter(entityId, "v-isRiftZoneKiller") then
           local entityPos = world.entityPosition(entityId)
-          local entityPosRelative = world.distance(entityPos, predictedPos)
           local displaySize = world.getObjectParameter(entityId, "killRange")
-          local displayPoly = {{-displaySize, -displaySize}, {displaySize, -displaySize}, {displaySize, displaySize}, {-displaySize, displaySize}}
-          localAnimator.addDrawable({
-            poly = displayPoly,
-            position = entityPosRelative,
-            color = displayColor
-          }, "ForegroundOverlay+10")
+          drawBox(entityPos, displaySize, dt)
         end
+      end
+
+      if world.entityAimPosition then
+        local aimPos = vec2.floor(world.entityAimPosition(entity.id()))
+        local displaySize = itemConfig.parameters.killRange or itemConfig.config.killRange
+
+        drawBox(aimPos, displaySize, dt)
       end
     end
   end
+end
+
+function drawBox(center, size, dt)
+  local predictedPos = vEntity.predictPosition(dt)
+
+  local centerRelative = world.distance(center, predictedPos)
+  local displayPoly = {{-size, -size}, {size, -size}, {size, size}, {-size, size}}
+  localAnimator.addDrawable({
+    poly = displayPoly,
+    position = centerRelative,
+    color = displayColor
+  }, "ForegroundOverlay+10")
 end
