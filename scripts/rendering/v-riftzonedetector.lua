4fd7ec3b225a2e765715886b84db0aace28041a7
LunarEyeGaming <lunareyegaming@gmail.com>
2026-08-12 22:58:21 -0400

    Detector uses riftzonemanager's tracker

--- scripts/rendering/v-riftzonedetector.lua
+++ scripts/rendering/v-riftzonedetector.lua
@@ -1,7 +1,16 @@
+require "/scripts/messageutil.lua"
 require "/scripts/rect.lua"
 
+require "/scripts/v-animator.lua"
+require "/scripts/v-time.lua"
+
+local WHITE = {255, 255, 255, 255}
+local ZERO_ALPHA_WHITE = {255, 255, 255, 0}
+
 local detectionRegion
-local canvas  ---@type CanvasWidget
+local ticker
+local promiseKeeper
+local riftZones
 
 local oldInit = init or function() end
 local oldUpdate = update or function() end
@@ -9,6 +18,15 @@ local oldUpdate = update or function() end
 function init()
   oldInit()
   detectionRegion = {-300, -300, 300, 300}
+  ticker = VTicker:new()
+  promiseKeeper = PromiseKeeper:new()
+  riftZones = {}
+
+  ticker:addInterval(0.1, function()
+    promiseKeeper:add(world.sendEntityMessage("v-riftzonemanager-stagehand", "getAllRiftZones"), function(res)
+      riftZones = res
+    end)
+  end)
 end
 
 function update(dt)
@@ -16,20 +34,19 @@ function update(dt)
 
   oldUpdate(dt)
 
-  local riftZones = world.getProperty("v-riftZones") or jarray()
+  ticker:update(dt)
+  promiseKeeper:update()
 
   local ownPos = world.entityPosition(player.id())
   for _, riftZone in ipairs(riftZones) do
     local pos = world.distance(riftZone.position, ownPos)
     if rect.contains(detectionRegion, pos) then
-      localAnimator.addDrawable({image = "/interface/scripted/v-riftzonedetector/point.png", position = vec2.mul(pos, 1 / 48), fullbright = true}, "ForegroundOverlay+256")
-    end
-  end
-
-  for _, entityId in ipairs(world.entityQuery(ownPos, 300, {includedTypes = {"monster"}})) do
-    if world.monsterType(entityId) == "v-riftzone" then
-      local pos = world.distance(world.entityPosition(entityId), ownPos)
-      localAnimator.addDrawable({image = "/interface/scripted/v-riftzonedetector/point.png", position = vec2.mul(pos, 1 / 48), fullbright = true}, "ForegroundOverlay+256")
+      localAnimator.addDrawable({
+        image = "/interface/scripted/v-riftzonedetector/point.png",
+        position = vec2.mul(pos, 1 / 48),
+        fullbright = true,
+        color = vAnimator.lerpColor(riftZone.timeToLiveRatio, ZERO_ALPHA_WHITE, WHITE)
+      }, "ForegroundOverlay+256")
     end
   end
 end
