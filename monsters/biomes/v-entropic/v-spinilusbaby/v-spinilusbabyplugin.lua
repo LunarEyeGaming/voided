0c6eea8603086d0bf4ed609ebc3a5f119ab08c70
LunarEyeGaming <lunareyegaming@gmail.com>
2026-08-12 22:12:23 -0400

    Spinilus Hatchling sprite update

--- monsters/biomes/v-entropic/v-spinilusbaby/v-spinilusbabyplugin.lua
+++ monsters/biomes/v-entropic/v-spinilusbaby/v-spinilusbabyplugin.lua
@@ -1,10 +1,5 @@
 require "/scripts/util.lua"
 
--- Wander state: Rotate to a random direction that won't make it run into a wall, then give a short boost.
--- Pursue state:
--- how tf do you do custom pathfinding???
---
-
 function turnAndBoost(direction, boostDelay, boostDuration, boostSpeed, waitTime)
   return function()
     mcontroller.controlFace(direction[1])
